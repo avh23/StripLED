2b8584b1ee15370d3c7626a2b18f88dc91ebd13c
Martin Schuster <Martin.Schuster1@infineon.com>
2017-11-11 08:19:48 +0100

    SRC: Interfacer: send cmds via Serial

--- Interfacer/Interfacer.ino
+++ Interfacer/Interfacer.ino
@@ -101,61 +101,87 @@ void handleIR(decode_results *results) {
         code &= ~0x0140; // cut "shift" bits
 
         if (code <=9 && ! repeat && ! shift) {
-            pattern = code;
+            sendCmd('p', code);
         }
 
     } else if (IR_type == 1) {
         // Alex's IR
         if (code == 0x807FF10E || code == 0x38D5F9F5) {
             // Flash
+            sendCmd('p', 1);
             pattern = 1;
         } else if (code == 0x807FE916 || code == 0xADEB659D) {
             // Strobe
+            sendCmd('p', 2);
             pattern = 2;
         } else if (code == 0x807FD926 || code == 0x92EBB33D) {
             // Fade
+            sendCmd('p', 3);
             pattern = 3;
         } else if (code == 0x807FC936 || code == 0x7B758561) {
             // Smooth
+            sendCmd('p', 4);
             pattern = 4;
         } else if (code == 0x9AF57A5F) {
             // off
-            pattern = 0;
-            setColor(0, 0, 0);
+            sendCmd('r', 0);
+            sendCmd('b', 1);
         } else if (code == 0x807FE11E || code == 0x7E087B19) {
             // on
-            pattern = 0;
-            setColor(100, 100, 100);
+            sendCmd('r', 1);
         }
     }
 
     irrecv.resume();
 }
 
+
+void sendCmd(char cmd, uint8_t param) {
+    /*
+       Send command to other ESP
+       Available commands:
+       b: blank (black)
+       r 0/1: stop/run
+       p x: set pattern x
+       s x: set color
+       d l c: directly set color c of led l
+     */
+    Serial.print('!');
+    Serial.print(cmd);
+    Serial.print(param);
+}
+
+void sendColor(uint32_t param) {
+    Serial.print( (param & (0x00ff0000)) >> 16);
+    Serial.print( (param & (0x0000ff00)) >>  8);
+    Serial.print( (param & (0x000000ff))      );
+}
+
+
 void showStatus(enum statuus s) {
     // simple way to show current status (OTA Update, AP config,...) with LEDs
 
-    // Serial.print("Status ");
-    // Serial.println(s);
-
-    fill_solid(leds, PixelCount, CRGB::Black);
+    sendCmd('r', 0);
+    sendCmd('b', 1);
 
     switch (s) {
         case OTA_START:
-            leds[61] = CRGB::Yellow;
+            sendCmd('d', 61);
+            sendColor(0xffff00);
             break;
         case OTA_ERROR:
-            leds[62] = CRGB::Red;
+            sendCmd('d', 62);
+            sendColor(0xff0000);
             break;
         case OTA_DONE:
-            leds[63] = CRGB::Green;
+            sendCmd('d', 63);
+            sendColor(0x00ff00);
             break;
         case AP_CONFIG:
-            leds[64] = CRGB::Blue;
+            sendCmd('d', 64);
+            sendColor(0x0000ff);
             break;
     }
 
-    reorderedShow();
-    delay(0.04*PixelCount);
 }
 
